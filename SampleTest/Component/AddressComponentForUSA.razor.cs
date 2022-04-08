8a2d24b400aab4d02ade733ee520bf4059e2fa9f
Pranav Zadafiya <pranavzadafiya@outlook.com>
2022-04-08 16:13:18 +0530

    CleanUp

--- SampleTest/Component/AddressComponentForUSA.razor.cs
+++ SampleTest/Component/AddressComponentForUSA.razor.cs
@@ -18,27 +18,30 @@ namespace SampleTest.Component
         private IJSRuntime _jsRuntime { get; set; }
 
         [Parameter]
+        public AppUserContextInfo contextInfo { get; set; }
+
+        [Parameter]
         public IPInfoResponse IPInfoResponse { get; set; }
 
         [Parameter]
         public EventCallback<IPInfoResponse> IPInfoResponseChanged { get; set; }
 
-
         public string _ipAddress { get; set; }
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-            IPInfoResponse = await GetIPInfo();
-        }
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            _ipAddress = await GetServerIPAddress();
+            if (firstRender)
+            {
+                _ipAddress = await GetServerIPAddress();
+                IPInfoResponse = await GetIPInfo();
+            }
         }
         public async Task<IPInfoResponse> GetIPInfo()
         {
             var token = ConfigSetting.IpLocationApiAccessToken;
             var host = ConfigSetting.IpLocationApiUrl;
             var ipInfoApi = new IpInfoApi(host, token);
+            if (string.IsNullOrWhiteSpace(_ipAddress))
+                return new IPInfoResponse();
             IPInfoResponse = await ipInfoApi.GetInformationByIpsAsync(_ipAddress);
             await IPInfoResponseChanged.InvokeAsync(IPInfoResponse);
             return IPInfoResponse;
@@ -46,13 +49,14 @@ namespace SampleTest.Component
 
         //public async Task onLookUpClick()
         //{
-        //	IPInfoResponse = await GetIPInfo();
+        //    IPInfoResponse = await GetIPInfo();
         //}
 
         public async Task<string> GetServerIPAddress()
         {
-           return await _jsRuntime.InvokeAsync<string>("GetAddress");
-
+            var ipAddress = await _jsRuntime.InvokeAsync<string>("getIpAddress")
+                    .ConfigureAwait(true);
+            return ipAddress;
         }
     }
 }
